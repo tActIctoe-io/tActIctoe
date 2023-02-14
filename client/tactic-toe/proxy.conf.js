c6085dffefd3c905670107bf858b85aaa75ac696
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-15 00:50:49 +0100

    x

--- client/tactic-toe/proxy.conf.js
+++ client/tactic-toe/proxy.conf.js
@@ -1,6 +1,6 @@
 const PROXY_CONFIG = {
   "/api": {
-      "target": "34.129.191.19:3000",
+      "target": "localhost:3000",
       "changeOrigin": true,
       "secure": false,
       "logLevel": "debug",
