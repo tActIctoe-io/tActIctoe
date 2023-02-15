986d5b8d1d530e30951dd5907febeb7f28a53fb8
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-15 23:45:37 +0100

    x

--- server/models/index.ts
+++ server/models/index.ts
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 
-const sequelize = new Sequelize('postgres://josepabellanapuyol:pass@database:5432/tactictoe');
+const sequelize = new Sequelize('postgres://user:pass@postgres:5432/db');
 const db:any = {};
 db.Users = require('./users')(sequelize, Sequelize.DataTypes);
 
