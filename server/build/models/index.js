2a30a62a45710dd5e670d6be5b688117e56c383d
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-15 23:47:32 +0100

    x

--- server/build/models/index.js
+++ server/build/models/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize('postgres://user:pass@database:5432/db');
+const sequelize = new Sequelize('postgres://user:pass@postgres:5432/db');
 const db = {};
 exports.db = db;
 db.Users = require('./users')(sequelize, Sequelize.DataTypes);
